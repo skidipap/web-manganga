a4124e10a6fc5ee2fa0f5812a78af657eb9c467c
mad-skidipap <mad.skidipap@gmail.com>
2021-03-12 15:59:39 +0700

    fix setting dev

--- manganga/settings.py
+++ manganga/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '9#rl&+s*l=&-t7^%&$-wle_2s%y4ti(!2yl3-c4$g5$+(i&6z)'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import os 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
