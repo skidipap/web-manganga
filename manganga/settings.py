81bc069c772e935015e8862200673f148f7a958e
mad-skidipap <mad.skidipap@gmail.com>
2021-03-13 10:56:37 +0700

    add static folder and skeleton css

--- manganga/settings.py
+++ manganga/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'manganga.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates/'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -123,4 +123,7 @@ STATIC_URL = '/static/'
 
 import os 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
