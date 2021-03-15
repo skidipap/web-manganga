e5fdebefc65e9fa6e1a95dcf2e7aaf466ed5da5d
mad-skidipap <mad.skidipap@gmail.com>
2021-03-15 08:27:12 +0700

    add author to models

--- mangareader/models.py
+++ mangareader/models.py
@@ -2,7 +2,12 @@ from django.db import models
 
 # Create your models here.
 class MangaList(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey(
+        'auth.User',
+        on_delete=models.CASCADE,
+        )
+    body = models.TextField()
 
     def __str__(self):
-        return self.title[:50]
+        return self.title
